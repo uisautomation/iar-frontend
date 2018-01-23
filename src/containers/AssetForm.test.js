466bc10b7ac0aa91d3583eadb2be4422152e1d64
Mike Bamford <mb2174@cam.ac.uk>
2018-01-23 12:44:10 +0000

    added helper test

--- src/containers/AssetForm.test.js
+++ src/containers/AssetForm.test.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { AppBar } from 'material-ui';
+import { render } from '../testutils';
 import AssetForm from './AssetForm';
 
 test('can render /asset/create', () => {
-  const testInstance = TestRenderer.create(
-    // TODO you should be able to use initialEntries/initialIndex - doesn't seem to add match property
-    <MemoryRouter>
-      <MuiThemeProvider>
-        <AssetForm match={{url: '/asset/create'}}/>
-      </MuiThemeProvider>
-    </MemoryRouter>
-  ).root;
+  const testInstance = render(<AssetForm match={{url: '/asset/create'}}/>);
 
   expect(testInstance.findByType(AppBar).props.title).toBe('Create new asset')
 });
