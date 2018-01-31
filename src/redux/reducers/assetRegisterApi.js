7b3fb15aaa7934edd644228199c2ec8b1f5ba082
Joseph Adams <jads92@hotmail.com>
2018-01-31 13:18:24 +0000

    Use spread operator in ASSETS_LIST_SUCCESS reducer

--- src/redux/reducers/assetRegisterApi.js
+++ src/redux/reducers/assetRegisterApi.js
@@ -7,9 +7,7 @@ const initialState = {
 export default (state = initialState, action) => {
   switch(action.type) {
     case ASSETS_LIST_SUCCESS:
-      return Object.assign({}, state, {
-        assets: action.payload.results
-      });
+      return {...state, assets: action.payload.results};
     case ASSETS_DELETE_SUCCESS:
       return {...state, assets: state.assets.filter((asset) => {
         return asset.url !== action.meta;
