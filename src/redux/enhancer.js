4f75326acc6758fbe0eb7a9603e5843ac5027e9d
Rich Wareham <rjw57@cam.ac.uk>
2018-02-02 11:50:47 +0000

    add authorisation headers to API requests
    Add some custom middleware which takes the token from the redux state
    and adds it to the authorisation headers of the HTTP response. For the
    moment we simply log the user out if the token is invalid or times out.
    For a more sophisticated UI, we might want to look at
    https://github.com/esbenp/redux-refresh-token to automate the refresh of
    the token.

--- src/redux/enhancer.js
+++ src/redux/enhancer.js
@@ -2,8 +2,9 @@ import { applyMiddleware } from 'redux';
 import { authMiddleware as auth } from 'redux-implicit-oauth2';
 import logger from 'redux-logger';
 import { apiMiddleware as api } from 'redux-api-middleware';
+import apiAuth from './middlewares/apiAuth';
 
-export const middlewares = [auth, api];
+export const middlewares = [auth, apiAuth, api];
 
 /**
  * Dispatch "enhancer" for redux. In our case it is simply the list of middlewares passed to
