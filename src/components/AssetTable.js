37c995449f5140b0797ffbdacef2336c771e4905
Rich Wareham <rich@richwareham.com>
2018-02-03 18:40:36 +0000

    add "zero view" for when there are no assets in the result
    Add a special ZeroAssetsRow component which is used to indicate that no
    assets are in the returned query. This is only displayed if a load is
    not currently in progress to avoid a flash of content.
    
    Closes #22.

--- src/components/AssetTable.js
+++ src/components/AssetTable.js
@@ -8,13 +8,14 @@ import {
   TableHeader,
   TableHeaderColumn,
   TableRow,
+  TableRowColumn,
 } from 'material-ui/Table';
 import AssetListItem from './AssetListItem';
 
 /**
  * A table of assets.
  */
-const AssetTable = ({ assetSummaries }) => (
+export const AssetTable = ({ assetSummaries, isLoadingAssets = false }) => (
   <div className="Asset-table">
     <Table
       fixedHeader={true}
@@ -38,6 +39,10 @@ const AssetTable = ({ assetSummaries }) => (
         displayRowCheckbox={false}
         className="Asset-table-body"
       >
+        {
+          // Display a "no assets" row if there is no loading happening and there are no assets.
+          ((assetSummaries.length === 0) && !isLoadingAssets) ? <ZeroAssetsRow /> : null
+        }
         { assetSummaries.map(({ url }) => <AssetListItem key={url} assetUrl={url} />) }
       </TableBody>
     </Table>
@@ -46,11 +51,27 @@ const AssetTable = ({ assetSummaries }) => (
 
 AssetTable.propTypes = {
   assetSummaries: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isLoadingAssets: PropTypes.bool,
 };
+
+/**
+ * Table row which is displayed when there are no assets in the current list.
+ */
+export const ZeroAssetsRow = () => (
+  <TableRow>
+    <TableRowColumn colSpan={6} style={{textAlign: 'center'}}>
+      There are no assets to display
+    </TableRowColumn>
+  </TableRow>
+);
 
 // Export unconnected version of component to ease testing
 export const UnconnectedAssetTable = AssetTable;
 
-const mapStateToProps = ({ assets }) => ({ assetSummaries: assets.summaries });
+const mapStateToProps = ({
+  assets: { summaries: assetSummaries, isLoading: isLoadingAssets }
+}) => ({
+  assetSummaries, isLoadingAssets
+});
 
 export default connect(mapStateToProps)(AssetTable);
