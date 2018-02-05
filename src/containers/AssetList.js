e32351bc1f98f3c9e5864e9211b4b0169ce757a5
Rich Wareham <rjw57@cam.ac.uk>
2018-02-05 17:42:02 +0000

    assetRegisterApi: add getAssets action which queries the asset list
    Add action which can take sort, filtering and search parameters which
    are passed to the asset API endpoint. Update the assets state with a
    query object which records the sort, filter and search parameters used.
    
    The default ordering is by descending updated at time. (I.e., the
    "freshest" objects are first.)

--- src/containers/AssetList.js
+++ src/containers/AssetList.js
@@ -1,10 +1,12 @@
-import React from 'react'; // used implicitly by JSX
+import React, { Component } from 'react'; // used implicitly by JSX
 import PropTypes from 'prop-types';
 import AssetListHeader from '../components/AssetListHeader';
 import AssetTable from '../components/AssetTable';
 import Page from '../containers/Page';
 import GetMoreAssets from '../components/GetMoreAssets';
 import { withRouter } from 'react-router'
+import { connect } from 'react-redux';
+import { getAssets, Direction } from '../redux/actions/assetRegisterApi';
 
 import '../style/App.css';
 
@@ -13,22 +15,42 @@ const TITLES = {
   '/assets/all': 'Assets: All',
 };
 
-const AssetList = ({ match }) => (
-  <Page>
-    <AssetListHeader title={TITLES[match.url]} />
+class AssetList extends Component {
+  componentDidMount() {
+    const { getAssets, lastAssetListUrl } = this.props;
 
-    {/* Table of currently loaded assets. */}
-    <AssetTable />
+    // Fetch an asset list if one has not already been fetched. We detect a fetch by looking at the
+    // "url" property in the assets state which is exposed as the "lastAssetListUrl" prop.
+    if(!lastAssetListUrl) {
+      getAssets({ sort: { field: 'updated_at', direction: Direction.descending } });
+    }
+  }
 
-    <div style={{textAlign: 'center'}}>
-      {/* When this component becomes visible more assets are loaded to populate the table. */}
-      <GetMoreAssets />
-    </div>
-  </Page>
-);
+  render() {
+    const { match } = this.props;
+    return (
+      <Page>
+        <AssetListHeader title={TITLES[match.url]} />
+
+        {/* Table of currently loaded assets. */}
+        <AssetTable />
+
+        <div style={{textAlign: 'center'}}>
+          {/* When this component becomes visible more assets are loaded to populate the table. */}
+          <GetMoreAssets />
+        </div>
+      </Page>
+    );
+  }
+};
 
 AssetList.propTypes = {
   match: PropTypes.object.isRequired,
+  lastAssetListUrl: PropTypes.string,
 };
 
-export default withRouter(AssetList);
+const mapStateToProps = ({ assets: { url: lastAssetListUrl } }) => ({ lastAssetListUrl });
+
+const mapDispatchToProps = { getAssets };
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(AssetList));
