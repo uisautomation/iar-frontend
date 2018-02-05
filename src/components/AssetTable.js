54dd26a4ca9155cb99c20f93f3ecf8d0564a353d
Rich Wareham <rich@richwareham.com>
2018-02-05 21:01:33 +0000

    enable sorting by clicking table headers
    Wire up sort to table headers. Re-factor table header and body into
    separate components to separate concerns: the body component need not
    know what the current query is and there's no point re-rendering the
    header when an asset is added/removed.

--- src/components/AssetTable.js
+++ src/components/AssetTable.js
@@ -1,60 +1,18 @@
 import React from 'react'; // used implicitly by JSX
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
-import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
-import AssetListItem from './AssetListItem';
+import Table from 'material-ui/Table';
+import AssetTableHeader from './AssetTableHeader';
+import AssetTableBody from './AssetTableBody';
 
 /**
  * A table of assets.
  */
-export const AssetTable = ({ assetSummaries, isLoadingAssets = false }) => (
+export const AssetTable = () => (
   <div className="Asset-table">
     <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Status</TableCell>
-          <TableCell>Department</TableCell>
-          <TableCell>Private</TableCell>
-          <TableCell>Last edited</TableCell>
-          <TableCell>&nbsp;</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody className="Asset-table-body">
-        {
-          // Display a "no assets" row if there is no loading happening and there are no assets.
-          ((assetSummaries.length === 0) && !isLoadingAssets) ? <ZeroAssetsRow /> : null
-        }
-        { assetSummaries.map(({ url }) => <AssetListItem key={url} assetUrl={url} />) }
-      </TableBody>
+      <AssetTableHeader />
+      <AssetTableBody />
     </Table>
   </div>
 );
 
-AssetTable.propTypes = {
-  assetSummaries: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isLoadingAssets: PropTypes.bool,
-};
-
-/**
- * Table row which is displayed when there are no assets in the current list.
- */
-export const ZeroAssetsRow = () => (
-  <TableRow>
-    <TableCell colSpan={6} style={{textAlign: 'center'}}>
-      There are no assets to display
-    </TableCell>
-  </TableRow>
-);
-
-// Export unconnected version of component to ease testing
-export const UnconnectedAssetTable = AssetTable;
-
-const mapStateToProps = ({
-  assets: { summaries: assetSummaries, isLoading: isLoadingAssets }
-}) => ({
-  assetSummaries, isLoadingAssets
-});
-
-export default connect(mapStateToProps)(AssetTable);
+export default AssetTable;
