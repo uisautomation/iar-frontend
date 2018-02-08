b3ed52f7b3d76bac1d22055db9bc8d4dda1b8eeb
Rich Wareham <rjw57@cam.ac.uk>
2018-02-08 10:48:49 +0000

    mocks: remove mocked Checkbox component
    MUI v1 no longer needs checkbox mocked for tests to pass.

--- src/test/mocks.js
+++ src/test/mocks.js
@@ -5,11 +5,6 @@ import React from 'react';
  * suite.
  */
 
-// need to mock the material-ui checkbox as we get 'TypeError: Cannot read property 'checked' of
-// undefined' when rendered with react-test-renderer. see
-// https://stackoverflow.com/questions/48465807/why-is-my-renderer-failing-when-using-material-ui-using-jest-and-react-test-rend
-jest.mock('material-ui/Checkbox', () => () => <input type='check' />);
-
 // GetMoreAssets needs mocking here because of the VisibilitySensor component it uses which requires
 // more of the DOM to be present than is provided by our test suite.
 jest.mock('../components/GetMoreAssets', () => () => <div />);
