7833c9eccd9abded5da726e2dd3655cfb725df6d
Rich Wareham <rjw57@cam.ac.uk>
2018-02-08 16:59:52 +0000

    add basic Sidebar styling
    Style the sidebar to be correctly positioned and sized. Arrange the menu
    as per the IAR design. The navigation is implemented via the Material UI
    List component but with a custom SidebarNavLink component which renders
    a link to static pages and styles itself appropriately when the currentl
    location matches the link.

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -5,7 +5,7 @@ import LoginButton from '../components/LoginButton';
  * A login page requesting that the user log in.
  */
 export default () => (
-  <div className="App-main">
+  <div>
     <h1>Welcome to the IAR</h1>
     <p>
       The information asset register is a way to record information about the University of
--- src/containers/Page.js
+++ src/containers/Page.js
@@ -2,13 +2,38 @@
  * Top-level page for IAR containing sidebar and content.
  */
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
 import { Sidebar } from '../components';
+import Drawer from 'material-ui/Drawer';
 
-export default ({ children }) => (
-  <div>
-    <Sidebar />
-    <div className="App-main">
+const drawerWidth = 240;
+
+const styles = theme => ({
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: drawerWidth,
+  },
+  pageContent: {
+    width: '100%',
+  },
+});
+
+const Page = ({ children, classes }) => (
+  <div className={classes.appFrame}>
+    <Drawer variant="permanent" classes={{paper: classes.drawerPaper}}>
+      <Sidebar />
+    </Drawer>
+    <div className={classes.pageContent}>
       { children }
     </div>
   </div>
 );
+
+export default withStyles(styles)(Page);
