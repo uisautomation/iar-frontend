00c377a433508b521dd880e78b58382cc77964e5
Rich Wareham <rjw57@cam.ac.uk>
2018-02-12 15:39:32 +0000

    stop delete confirmation dialog flashing
    As noted in #41, the delete confirmation dialog flashes when dismissed.
    Re-work the delete confirmation flow to keep the asset URL around so
    that it can be used to keep the delete confirmation dialog populated as
    it fades away.
    
    Closes #41

--- src/redux/reducers/deleteConfirmation.js
+++ src/redux/reducers/deleteConfirmation.js
@@ -3,17 +3,20 @@ import { CONFIRM_DELETE, HANDLE_CONFIRM_DELETE } from '../actions/deleteConfirma
 export const initialState = {
   // If not null, this is the URL of an asset which is being considered for deletion.
   url: null,
+
+  // If true, the user should be prompted to confirm deletion of the asset.
+  userShouldConfirm: false,
 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case CONFIRM_DELETE:
-      return {...state, url: action.payload.url};
+      return { ...state, url: action.payload.url, userShouldConfirm: true };
     case HANDLE_CONFIRM_DELETE:
       // only actually modify the state if the url in the action payload matches the one being
       // confirmed
       if(action.payload.url === state.url) {
-        return {...state, url: null };
+        return { ...state, userShouldConfirm: false };
       } else {
         return state;
       }
--- src/redux/reducers/deleteConfirmation.test.js
+++ src/redux/reducers/deleteConfirmation.test.js
@@ -9,16 +9,29 @@ test('initial state has unset url', () => {
   expect(reducer(undefined, {}).url).toBeNull();
 });
 
+test('initial state does not ask for confirmation', () => {
+  expect(reducer(undefined, {}).userShouldConfirm).toBe(false);
+});
+
 test('asking for delete confirmation sets URL', () => {
   expect(reducer(undefined, confirmDelete('xxx')).url).toBe('xxx');
 });
 
-test('handling delete clears URL', () => {
-  const state = { ...reducer(undefined, {}), url: 'xxx' };
-  expect(reducer(state, handleConfirmDelete('xxx')).url).toBeNull();
+test('asking for delete confirmation sets userShouldConfirm', () => {
+  expect(reducer(undefined, confirmDelete('xxx')).userShouldConfirm).toBe(true);
+});
+
+test('handling delete leaves URL', () => {
+  const state = { ...reducer(undefined, {}), url: 'xxx', userShouldConfirm: true };
+  expect(reducer(state, handleConfirmDelete('xxx')).url).toBe('xxx');
+});
+
+test('handling delete clears userShouldConfirm', () => {
+  const state = { ...reducer(undefined, {}), url: 'xxx', userShouldConfirm: true };
+  expect(reducer(state, handleConfirmDelete('xxx')).userShouldConfirm).toBe(false);
 });
 
 test('handling delete with wrong URL does not change state', () => {
-  const state = { ...reducer(undefined, {}), url: 'xxx' };
+  const state = { ...reducer(undefined, {}), url: 'xxx', userShouldConfirm: true };
   expect(reducer(state, handleConfirmDelete('yyy'))).toBe(state);
 });
