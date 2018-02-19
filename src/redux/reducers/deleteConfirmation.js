c01288aaf40a7715706f057da447e40b753c50eb
Rich Wareham <rjw57@cam.ac.uk>
2018-02-19 16:13:43 +0000

    re-work delete confirmation to return promise
    Dispatching the confirmDelete action now returns a Promise which is
    resolved/rejected with the URL depending on whether the user
    confirms/cancels the deletion.
    
    The reasoning for this is that the delete asset button on the asset edit
    form needs to wait for the confirmation to go through before deciding
    whether to navigate away from the page.

--- src/redux/reducers/deleteConfirmation.js
+++ src/redux/reducers/deleteConfirmation.js
@@ -6,16 +6,28 @@ export const initialState = {
 
   // If true, the user should be prompted to confirm deletion of the asset.
   userShouldConfirm: false,
+
+  // On confirmation of delete, this callback should be called with the URL of the deleted asset
+  // and a flag indicating if the user confirmed delete.
+  onHandleConfirmDelete: () => null,
 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case CONFIRM_DELETE:
-      return { ...state, url: action.payload.url, userShouldConfirm: true };
+      return {
+        ...state,
+        url: action.payload.url,
+        onHandleConfirmDelete: action.payload.onHandleConfirmDelete,
+        userShouldConfirm: true,
+      };
     case HANDLE_CONFIRM_DELETE:
       // only actually modify the state if the url in the action payload matches the one being
       // confirmed
       if(action.payload.url === state.url) {
+        if(state.onHandleConfirmDelete) {
+          state.onHandleConfirmDelete(action.payload.userApprovedRequest);
+        }
         return { ...state, userShouldConfirm: false };
       } else {
         return state;
