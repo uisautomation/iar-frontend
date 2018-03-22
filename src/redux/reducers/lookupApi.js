104fbb7b94c48b1e7d3098ece1a0e3dfcc21d82b
Rich Wareham <rjw57@cam.ac.uk>
2018-03-22 14:45:19 +0000

    re-work lookupApi institutions fetch to avoid re-fetch on error
    Since there was no memory in the redux state about whether fetching an
    institution list *failed*, we could get into a situation where the
    institutions are requested, the request fails and the request is
    re-issued hammering the browser in the interim.

--- src/redux/reducers/lookupApi.js
+++ src/redux/reducers/lookupApi.js
@@ -3,7 +3,7 @@ import {
   PEOPLE_GET_SELF_REQUEST, PEOPLE_GET_SELF_RESET, PEOPLE_GET_SELF_SUCCESS,
   PEOPLE_GET_SUCCESS,
   PEOPLE_LIST_SUCCESS,
-  INSTITUTIONS_LIST_SUCCESS,
+  INSTITUTIONS_LIST_REQUEST, INSTITUTIONS_LIST_SUCCESS, INSTITUTIONS_LIST_FAILURE
 } from '../actions/lookupApi';
 
 import Cache from '../cache';
@@ -21,12 +21,23 @@ export const initialState = {
 
   // the authenticated user's profile
   self: null,
+
   // whether or not the authenticated user's profile is being loaded
   selfLoading: false,
 
   institutions: {
     // If non-NULL, a JS Date object indicating when this was last fetched.
     fetchedAt: null,
+
+    // If non-NULL, a JS Date object indicating when this was last requested.
+    requestedAt: null,
+
+    // If true, a request is currently in flight.
+    isLoading: false,
+
+    // If non-null, the last request failed and this contains the payload from the
+    // INSTITUTIONS_LIST_FAILURE action.
+    errorPayload: null,
 
     // A map of all institution records keyed by institution id.
     byInstid: ImmutableMap(),
@@ -59,16 +70,41 @@ export default (state = initialState, action) => {
       // Add the person to the peopleByCrsid map
       return { ...state, self: action.payload, selfLoading: false};
 
+    case INSTITUTIONS_LIST_REQUEST:
+      // An institution request is in flight.
+      return {
+        ...state,
+        institutions: {
+          ...state.institutions,
+          isLoading: true,
+          requestedAt: new Date(),
+          fetchedAt: null,
+          errorPayload: null,
+        },
+      };
+
     case INSTITUTIONS_LIST_SUCCESS:
       // Replace current institution list with returned results
       return {
         ...state,
         institutions: {
           ...state.institutions,
+          isLoading: false,
           fetchedAt: new Date(),
+          errorPayload: null,
           byInstid: ImmutableMap(action.payload.results.map(
             institution => [institution.instid, institution]
           )),
+        },
+      };
+
+    case INSTITUTIONS_LIST_FAILURE:
+      return {
+        ...state,
+        institutions: {
+          ...state.institutions,
+          isLoading: false,
+          errorPayload: action.payload,
         },
       };
 
