97c14b80979689ffa57c853ab47478f9afbd3670
Mike Bamford <mb2174@cam.ac.uk>
2018-04-03 09:58:00 +0100

    Merge branch 'master' into navigation_logic_inconsistent
    # Conflicts:
    #	src/containers/AssetList.js

--- src/containers/AssetList.test.js
+++ src/containers/AssetList.test.js
@@ -56,7 +56,7 @@ test('AssetList respects the current query', () => {
     ...initialState.assets.query,
     sort: { field: 'name', direction: Direction.descending }
   };
-  initialState.assets = { ...initialState.assets, query: initialQuery, fetchedAt: new Date() };
+  initialState.assets = { ...initialState.assets, query: initialQuery };
 
   const store = createMockStore(initialState);
   render(<AppRoutes />, { store, url: '/assets', store });
